6ade7745f52be2a557bfa14581dec4458b136474
almileo <leonardo.almiron@tutenlabs.com>
2023-01-16 22:00:43 -0300

    Primer entrega. Proyecto bien basico de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
